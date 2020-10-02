d98fb100d095402b2ec3723ae94a566b819ae9f3
Nicolas <nec.catalano@gmail.com>
2020-10-02 00:22:04 -0300

    Generacion de las intancias necesarias

--- clases.py
+++ clases.py
@@ -1,4 +1,3 @@
-
 class PartidoPolitico():
     def __init__(self, nombre):
         self.nombre = nombre
@@ -7,12 +6,21 @@ class PartidoPolitico():
         return "El partido politico se llama {}".format(self.nombre)
 
 
+class Direccion():
+    def __init__(self, calle, num, barrio):
+        self.calle = calle
+        self.num = num
+        self.barrio = barrio
+
+
 class Persona():
-    def __init__(self, nombre, apellido, dni, edad):
+    def __init__(self, nombre, apellido, dni, edad, direccion):
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.edad = edad
+        self.direccion = direccion
+
 
 
 class Fecha():
@@ -22,17 +30,15 @@ class Fecha():
         self.anio = anio
 
 class Padron():
-    def __init__(self, numMesa, lugar, seccion, fecha):
+    def __init__(self, numMesa, lugar, fecha):
         self.numMesa = numMesa
         self.lugar = lugar
-        self.seccion = seccion
         self.fecha = fecha
     
     def __str__(self):
-        return "Número mesa: {}, Lugar: {}, Seccion: {}, fecha: {}/{}/{}".format(
+        return "Número mesa: {}, Lugar: {}, fecha: {}/{}/{}".format(
             self.numMesa,
             self.lugar,
-            self.seccion,
             self.fecha.dia,
             self.fecha.mes,
             self.fecha.anio,
@@ -40,31 +46,32 @@ class Padron():
 
 
 class Votante(Persona):
-    def __init__(self, nombre, apellido, dni, edad, voto, padronElectoral):
-        super().__init__(nombre, apellido, dni, edad)
+    def __init__(self, nombre, apellido, dni, edad, direccion, voto, padronElectoral):
+        super().__init__(nombre, apellido, dni, edad, direccion)
         self.voto = voto
         self.padronElectoral = padronElectoral
 
     def __str__(self):
-        return "Nombre: {}, Apellido: {}, DNI: {}, Edad: {}, Voto: {}, \n---Padron Electoral---\n {}".format(
+        return "Nombre: {}, Apellido: {}, DNI: {}, Edad: {}, Dirección: {} {}/{}, Voto: {}, \n---Padron Electoral---\n {}".format(
             self.nombre,
             self.apellido,
             self.dni,
             self.edad,
+            self.direccion.calle,
+            self.direccion.num,
+            self.direccion.barrio,
             self.voto,
             self.padronElectoral,
             )
 
 class Candidato(Votante):
-    def __init__(self, nombre, apellido, dni, edad, voto, padronElectoral, titulo, puesto, titular):
-        super().__init__(nombre, apellido, dni, edad, voto, padronElectoral)
-        self.titulo = titulo
+    def __init__(self, nombre, apellido, dni, edad, direccion, voto, padronElectoral, puesto, titular):
+        super().__init__(nombre, apellido, dni, edad, direccion, voto, padronElectoral)
         self.puesto = puesto
         self.titular = titular
 
     def __str__(self):
-        return  super().__str__() + "\nTitulo: {}, Puesto: {}, titular?: {}".format(
-            self.titulo,
+        return  super().__str__() + "\nPuesto: {}, titular?: {}".format(
             self.puesto,
             self.titular,
             )
@@ -92,6 +99,13 @@ class Voto():
         self.impugnado = impugnado
         self.valido = valido
     
+    def __str__(self):
+        return  "Voto a lista: {}, \n¿Voto blanco?: {}, \n¿Voto impugnado?: {}, \n¿Voto valido?: {},".format(
+            self.lista,
+            self.blanco,
+            self.impugnado,
+            self.valido,
+            )
 
 class SistemaVotacion():
     def __init__(self, listas, votantes, fecha, votos):
@@ -99,6 +113,43 @@ class SistemaVotacion():
         self.votantes = votantes
         self.fecha = fecha
         self.votos = votos
+
+    def controlVotaciones(self):
+        votosBlancos = 0
+        votosImpugnados = 0
+        votosValidos = 0
+        for voto in self.votos:
+            if voto.blanco:
+                votosBlancos += 1
+            elif voto.impugnado:
+                votosImpugnados += 1
+            else:
+                votosValidos += 1
+        return [votosBlancos, votosImpugnados, votosValidos]
+    
+    def porcentajeCandidato(self):
+        listaPorcentaje = [0,0,0,0]
+        
+        for voto in self.votos:
+            if voto.valido:
+                if voto.lista.numLista == self.listas[0].numLista:
+                    listaPorcentaje[0] += 1
+                elif voto.lista.numLista == self.listas[1].numLista:
+                    listaPorcentaje[1] += 1
+                elif voto.lista.numLista == self.listas[2].numLista:
+                    listaPorcentaje[2] += 1
+                else:
+                    listaPorcentaje[3] += 1
+        
+        for i in range(len(listaPorcentaje)):
+            listaPorcentaje[i] = ((listaPorcentaje[i] * 100) / len(self.votos))
+
+
+        return listaPorcentaje
+        
+    def buscarGanador(self, lista):
+        pass
+
 
 
 
--- main.py
+++ main.py
@@ -1,30 +1,168 @@
 from clases import *
+from random import randint
 
-PARTIDOS_POLITICOS = [
-    PartidoPolitico("Juntos por el cambio"),
-	PartidoPolitico("Frente de todos"),
-	PartidoPolitico("Consenso Federal"),
-	PartidoPolitico("Unite"),
-    PartidoPolitico("Fit unidad"),
- 	PartidoPolitico("Frente nos"),
+
+FECHA = Fecha(15, 5, 2019)
+
+
+DIRECCIONES = [
+	# BARRIO LA BLORIA
+	Direccion("Rawson", 0, "La Gloria"),
+	Direccion("Puerto San Jose", 0, "La Gloria"),
+	Direccion("Bahía Aguirre", 0, "La Gloria"),
+	Direccion("Puerto Concepción", 0, "La Gloria"),
+	Direccion("Bahía Ushuaia", 0, "La Gloria"),
+	Direccion("Puerto año nuevo", 0, "La Gloria"),
+	# BARRIO FUCH
+	Direccion("Laguna Palena", 0, "Fuch"),
+	Direccion("Lago Hermoso", 0, "Fuch"),
+	Direccion("Lago Correntoso", 0, "Fuch"),
+	Direccion("Laguna de la esquina", 0, "Fuch"),
+	Direccion("Laguna de la Niña Encantada", 0, "Fuch"),
+	Direccion("Laguna del Toro", 0, "Fuch"),
+	# BARRIO LA ESTANZUELA
+	Direccion("Telteca", 0, "La Estanzuela"),
+	Direccion("Tilcara", 0, "La Estanzuela"),
+	Direccion("Payer", 0, "La Estanzuela"),
+	Direccion("Mapuche", 0, "La Estanzuela"),
+	Direccion("Trenque Lauquen", 0, "La Estanzuela"),
+	Direccion("Yupanqui", 0, "La Estanzuela"),
+	# BARRIO GOBERNADOR BENEGAS
+	Direccion("Artigas", 0, "Gobernador Benegas"),
+	Direccion("Roffo A.", 0, "Gobernador Benegas"),
+	Direccion("Hilario Cuadros", 0, "Gobernador Benegas"),
+	Direccion("Gargilo", 0, "Gobernador Benegas"),
+	Direccion("Feliciani", 0, "Gobernador Benegas"),
+	Direccion("Alfaro", 0, "Gobernador Benegas"),
+	# BARRIO SAN VICENTE
+	Direccion("Dique Tulumaya", 0, "San Vicente"),
+	Direccion("Chile", 0, "San Vicente"),
+	Direccion("El Nihuil", 0, "San Vicente"),
+	Direccion("Alpatacal", 0, "San Vicente"),
+	Direccion("Los Jacatanda", 0, "San Vicente"),
+	Direccion("Las Casuarinas", 0, "San Vicente"),
+	# BARRIO VILLA HIPÓDROMO
+	Direccion("Pismata", 0, "Villa Hipódromo"),
+	Direccion("El volcan", 0, "Villa Hipódromo"),
+	Direccion("El Vergel", 0, "Villa Hipódromo"),
+	Direccion("Cosquín", 0, "Villa Hipódromo"),
+	Direccion("Pedernera", 0, "Villa Hipódromo"),
+	Direccion("América", 0, "Villa Hipódromo"),
 ]
 
-# for partido in PARTIDOS_POLITICOS:
-#     print(partido)
+NOMBRES = [
+	"Nicolas",
+	"Jose",
+	"Manuel",
+	"Francisco",
+	"Ciro",
+	"Maria",
+	"Noemi",
+	"Lucia",
+	"Teresa",
+	"Natalia",
+]
 
-padron = Padron(25, "Escuela Villanueva", 15, Fecha(15, 5, 2019))
-padron1 = Padron(45, "Escuela Mitre", 105, Fecha(15, 5, 2019))
+APELLIDOS =[
+	"Catalano",
+	"Salimeni",
+	"Coria",
+	"Figueroa",
+	"Muratore",
+	"Ortiz",
+	"Perez",
+	"Gonzales",
+	"Matus",
+	"Perez",
+]
 
-votante = Votante("Nicolas", "Catalano", 38760722, 25, False, padron)
+def controlBarrio(barrio):
+	""" Función que permite asignar una escuela para la votación segun el barrio de residencia """
 
-print(votante)
-print("--------------------------")
-albertoF = Candidato("Alberto", "Fernadez", 1165789, 99, False, padron1, "Abogado", "Presidente", True)
+	if barrio == "La Gloria":
+		return "Esc Padre Pedro Arce"
 
-print(albertoF)
-print("--------------------------")
+	elif barrio == "Fuch":
+		return "Esc Reyes Catolico"
 
-lista1 = Lista(PARTIDOS_POLITICOS[0], 508, albertoF)
-print(lista1)
+	elif barrio == "La Estanzuela":
+		return "Esc Profesor Geronimo Sosa"
+
+	elif barrio == "Gobernador Benegas":
+		return "Esc Misiones"
+
+	elif barrio == "San Vicente":
+		return "Esc DR. Americo Cali"
+
+	else:
+		return "Esc Victoria Ocampo"
+
+def generarVotantes():
+	votantes = []
+	for i in range(500):
+		# Declaro variables con valores aleatorios
+		nombre = NOMBRES[randint(0,(len(NOMBRES)-1))]
+		apellido = APELLIDOS[randint(0,(len(APELLIDOS)-1))]
+		direccion = DIRECCIONES[randint(0,(len(DIRECCIONES)-1))]
+		padron = Padron(randint(0, 1000), controlBarrio(direccion.barrio), FECHA)
+		direccion.num = randint(0, 1000)
+
+		# Cargo el array con el objeto
+		votantes.append(Votante(nombre, apellido, randint(5000000, 48000000), randint(18, 99), direccion, False, padron))
+	
+	return votantes
+
+votantes = generarVotantes()
+
+# for votante in votantes:
+# 	print(votante)
+# 	print("-------------------------")
 
 
+def generarCandidatos():
+	listas = []
+	tadeo = Candidato("Tadeo", "Zalazar", 35025896, 35, DIRECCIONES[21], False, controlBarrio(DIRECCIONES[21].barrio), "Intendente", True)
+	listas.append(Lista(PartidoPolitico("Cambia Mendoza"), 503, tadeo))
+
+	andrea  = Candidato("Andrea", "Blandini", 55078897, 55, DIRECCIONES[26], False, controlBarrio(DIRECCIONES[26].barrio), "Intendente", True)
+	listas.append(Lista(PartidoPolitico("Eleguí Mendoza"), 43, andrea))
+
+	hector = Candidato("Héctor", "Fresina", 29634785, 30, DIRECCIONES[15], False, controlBarrio(DIRECCIONES[15].barrio), "Intendente", True)
+	listas.append(Lista(PartidoPolitico("FIT"), 786, hector))
+
+	leonardo = Candidato("Leonardo", "Verdini", 47568932, 45, DIRECCIONES[30], False, controlBarrio(DIRECCIONES[30].barrio), "Intendente", True)
+	listas.append(Lista(PartidoPolitico("Protectora"), 105, leonardo))
+
+	return listas
+
+listas = generarCandidatos()
+
+# for lista in listas:
+# 	print(lista)
+# 	print("------------------")
+
+
+
+def generarVotos():
+	listaVotos = []
+	numAlAzar = 0
+	for i in range(500):
+		numAlAzar = randint(1,3)
+		if numAlAzar == 1:
+			listaVotos.append(Voto(listas[randint(0,3)],True, False, False))
+		elif numAlAzar == 2:
+			listaVotos.append(Voto(listas[randint(0,3)],False, True, False))
+		else:
+			listaVotos.append(Voto(listas[randint(0,3)],False, False, True))
+	
+	return listaVotos
+
+listaVotos = generarVotos()
+# for lista in listaVotos:
+# 	print(lista)
+# 	print("------------------")
+
+sistemaVotacion = SistemaVotacion(listas, votantes, FECHA, listaVotos)
+
+print(sistemaVotacion.controlVotaciones())
+print(sistemaVotacion.porcentajeCandidato())
